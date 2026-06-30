04b90cc859f0c2b198b3abed4ef497907f548199
BD4SUR <20069428+bd4sur@users.noreply.github.com>
2026-06-30 20:22:53 +0800

    2026-06-30

--- test/calendar.scm
+++ test/calendar.scm
@@ -1,29 +1,3 @@
-
-(define A
-  (lambda (k x1 x2 x3 x4 x5)
-      (define B
-        (lambda ()
-            (set! k (- k 1))
-            (A k B x1 x2 x3 x4)))
-      (if (<= k 0)
-          (+ (x4) (x5))
-          (B))))
-
-(define thunk_1  (lambda () 1))
-(define thunk_m1 (lambda () -1))
-(define thunk_0  (lambda () 0))
-
-(define MoB
-  (lambda (x)
-    (display "Man or Boy Test")(newline)
-    (display "A[10] = ") (display (A x thunk_1 thunk_m1 thunk_m1 thunk_1 thunk_0)) (display " / 期望-67") (newline)
-    (display "A[11] = ") (display (A (+ x 1) thunk_1 thunk_m1 thunk_m1 thunk_1 thunk_0)) (display " / 期望-138") (newline)
-    (display "A[12] = ") (display (A (+ x 2) thunk_1 thunk_m1 thunk_m1 thunk_1 thunk_0)) (display " / 期望-291") (newline)
-    (newline)
-))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Animac测试用例
 ;; 2012.6    C语言编写
@@ -116,3 +90,9 @@
     (print-calendar year month)
     (display "====================")(newline)
   ))
+
+(define run
+  (lambda () {
+    (Calendar 2026 7)
+  })
+)
